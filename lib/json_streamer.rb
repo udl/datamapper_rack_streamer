0b599d43d0f283137d891e2739be6d17a4c277ec
Udo Groebner <ugroebner@crealytics.de>
2010-07-28 14:03:29 +0200

    Simplifications.

--- lib/json_streamer.rb
+++ lib/json_streamer.rb
@@ -4,21 +4,11 @@ class JsonStreamer
   include Enumerable
 
   attr_reader :json_sequence
-  def initialize(model_class,ids,json_sequence = nil, per_page = 1000, filter_mappings = {}, &block)
+  def initialize(model_class,ids, per_page = 1000, &block)
     @ids = ids
     @model_class = model_class
-    @json_sequence = json_sequence || self.class.json_sequence(@model_class)
     @per_page = per_page
-    @filter_mappings = filter_mappings
     @block = block
-  end
-
-  def self.json_sequence(model_class)
-    model_class.properties.collect {|p| p.name} - [:id]
-  end
-
-  def to_json(array)
-    array.to_json
   end
 
   def each
@@ -29,8 +19,7 @@ class JsonStreamer
       elements = @model_class.all(:id=>@ids[page_counter*@per_page,@per_page])
       elements.each do |element|
         element = @block.call(element) unless @block.nil?
-        json = element.to_json
-        yield StringIO.new(json).read
+        yield element.to_json
         yield "," unless element == elements.last
       end
       page_counter = page_counter + 1
