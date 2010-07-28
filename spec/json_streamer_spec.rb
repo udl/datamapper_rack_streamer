f1dba8b7a8dc15250e6dfd14452ba77dc6d56112
Udo Groebner <ugroebner@crealytics.de>
2010-07-28 15:27:01 +0200

    Fix test for JsonStreamer header and footer handling.

--- spec/json_streamer_spec.rb
+++ spec/json_streamer_spec.rb
@@ -6,12 +6,12 @@ describe JsonStreamer do
   describe "with a provided header and footer" do
     before do
       @model_class = Product
-      Product.create(:id => 1, :title => "Hans")
-      Product.create(:id => 4, :title => "Hans")
-      Product.create(:id => 5, :title => "Hans")
+      Product.create(:id => 1, :title => "Hans1", :shop_id => 1)
+      Product.create(:id => 4, :title => "Hans4", :shop_id => 1)
+      Product.create(:id => 5, :title => "Hans5", :shop_id => 1)
       @ids = Product.all.map{|p| p.id}
-      @header = "{items: "
-      @footer = "}"
+      @header = '{"items": '
+      @footer = '}'
     end
     it "should apply the header and footer to the response" do
       streamer = JsonStreamer.new(Product, @ids, 1, @header, @footer)
@@ -19,6 +19,7 @@ describe JsonStreamer do
         strg = strg + elem
       end
       result_string.should == @header + Product.all.to_json + @footer
+      JSON.parse(result_string)
     end
   end
 end
