a7945c3fa93cad69740b4a89c2045d10fc9edab1
Udo Groebner <ugroebner@crealytics.de>
2010-07-28 17:03:18 +0200

    JsonStreamer bugfix.

--- lib/json_streamer.rb
+++ lib/json_streamer.rb
@@ -21,9 +21,10 @@ class JsonStreamer
     while (page_counter * @per_page <= elements_count)  do
       elements = @model_class.all(:id=>@ids[page_counter*@per_page,@per_page])
       elements.each do |element|
+        is_last_element = element.id == @ids.last
         element = @block.call(element) unless @block.nil?
         yield element.to_json
-        yield "," unless element.id == @ids.last
+        yield "," unless is_last_element
       end
       page_counter = page_counter + 1
     end
